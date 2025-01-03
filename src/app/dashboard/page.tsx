767e39fbe4cd678bb2291de0b5633f09d40afcb1
Dong <k30110106@gmail.com>
2025-01-04 02:07:02 +0900

    commit files

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import variables from "../scss/variables.module.scss";
+
+const page = () => {
+  return (
+    <div>
+      <h1 style={{ color: variables.primaryColor }}>DASHBOARD</h1>
+    </div>
+  );
+};
+
+export default page;
